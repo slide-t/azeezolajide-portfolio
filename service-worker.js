ccfb9b3b1403acde85d455ac58837bbfa2d341d2
Alimi Azeez Olajide <slideteckacademy@gmail.com>
2025-08-21 18:18:59 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "portfolio-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+// Install & cache
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate & clean old cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch handler
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          if (event.request.mode === "navigate") {
+            return caches.match("/index.html");
+          }
+        })
+      );
+    })
+  );
+});
